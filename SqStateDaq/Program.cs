06adb51143e152f245f9d32441340c9d220f018d
JingYu Ning <foldfelis@gmail.com>
2021-08-31 00:11:12 +0800

    update process

--- SqStateDaq/Program.cs
+++ SqStateDaq/Program.cs
@@ -10,11 +10,12 @@ namespace SqStateDaq
             var baseAddress = new Uri("http://127.0.0.1:8080/");
             var serviceHost = new WebServiceHost(typeof(SqDataQueryServices), baseAddress);
             serviceHost.Open();
-            Console.WriteLine("Web is running");
-            Console.WriteLine("Press any key to stop");
-            Console.ReadKey();
+            Console.WriteLine("DAQ is running");
+            Console.WriteLine("Enter `quit` any key to stop");
+            while (Console.ReadLine() != "quit") { }
             serviceHost.Close();
 
+            Console.WriteLine("DAQ stopped, press any key to exit");
             Console.ReadLine();
         }
     }
